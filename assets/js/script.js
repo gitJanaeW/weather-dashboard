054b4d59fca3513431a6a814e8f5abb618c54e4e
janae.wel <janae.wel@gmail.com>
2022-07-03 11:13:08 -0400

    condensing query selectors

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,27 @@
+// VARIABLES
+// Selectors
 var forecastSectionEl = document.querySelector(".forecast-section");
 var searchBtnEl = document.querySelector(".search-btn");
 var inputEl = document.querySelector(".input");
+var cityInfoEl = document.querySelector(".city-info");
+var cityStatsEl = document.querySelector(".city-stats");
+var cityNameEl = document.querySelector(".city-name");
+var cityDateEl = document.querySelector(".city-date");
+var cityStatsEl = document.querySelector(".city-stats");
 
+var emptyMainEl = document.createElement("div");
+var input = undefined;
+// Global elements
+var promptEl = document.createElement("p");
+var allCityDetails = undefined;
+// Checkers
+var emptyPage = false;
+// time and date
 var m = moment().format("dddd, MMMM Do, YYYY, h:mm a");
 
 
 // PROMPT USER TO INPUT CITY
 function createEmptyPage(){
-    // create new div with a cloud image and prompt message
-    var emptyMainEl = document.createElement("div");
     forecastSectionEl.appendChild(emptyMainEl);
 
     var cloudEl = document.createElement("img");
@@ -16,30 +29,74 @@ function createEmptyPage(){
     cloudEl.setAttribute("src", "./assets/images/clouds.png"); // Image source: https://www.iconsdb.com/gray-icons/clouds-2-icon.html
     emptyMainEl.appendChild(cloudEl);
 
-    var promptEl = document.createElement("p");
     promptEl.className = "prompt-text";
     promptEl.textContent = "Select a city name to see the forecast.";
     emptyMainEl.appendChild(promptEl);
+    
+    emptyPage = true;
 }
 
 // GET DESIRED CITY
 function getCityInput(e){
     e.preventDefault();
 
-    var input = inputEl.value.trim();
+    input = inputEl.value.trim();
     inputEl.value = "";
     if(input === ""){
         return;
     }else{
-        getCityWeather(input);
-        console.log(moment().format("dddd, MMMM Do, YYYY, h:mm a"));
+        fetchCityWeather(input);
+        console.log(m);
     }
 }
 
-function getCityWeather(cityName){
-    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=" + "43.65" + "&lon=" + "79.3" + "&dt=" + "minutely" + "&appid=" + "{20d2e8b234ab2aab32059e9826a39af0}";
+// FETCH API AND PARSE RESULTS
+function fetchCityWeather(cityName){
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=20d2e8b234ab2aab32059e9826a39af0";
     console.log("apiUrl", apiUrl);
+    fetch(apiUrl).then(function(response){
+        if(response.ok){
+            // Parse the response (data) with json
+            response.json().then(function(data){
+                // save data globally
+                allCityDetails = data;
+                // push that data into getResults()
+                returnResults(data);
+            });
+        }else{
+            promptEl.textContent = "Error: City Name Not Found.";
+            promptEl.style.fontStyle =  "italic";
+            promptEl.style.fontWeight = "bold";
+        }
+    });
+    
 }
+
+// GET THE RESULTS FOR THE CITY SELECTED
+function returnResults(cityResults){
+    printCityInfo();
+    printFiveDayForecast();
+    console.log("long", cityResults.coord.lon);
+}
+
+function printCityInfo(){
+    if(emptyPage){
+        forecastSectionEl.removeChild(emptyMainEl);
+    }
+    cityInfoEl.removeAttribute("hidden");
+    // create city header info
+    cityNameEl.textContent = allCityDetails.name;
+    cityDateEl.textContent = m;
+    // create city stats
+    for(var i = 0; i < cityStatsEl.childElementCount; i++){
+        
+    }
+}
+
+function printFiveDayForecast(){
+    console.log("print five day forecast");
+}
+
 
 createEmptyPage();
 
