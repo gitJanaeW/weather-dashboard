2d0961b6c0ab601bd1e50be7f80fee513cd7f277
janae.wel <janae.wel@gmail.com>
2022-07-03 09:08:30 -0400

    initial OpenWeather API key

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,9 @@
 var forecastSectionEl = document.querySelector(".forecast-section");
 var searchBtnEl = document.querySelector(".search-btn");
-inputEl = document.querySelector(".input");
+var inputEl = document.querySelector(".input");
+
+var m = moment().format("dddd, MMMM Do, YYYY, h:mm a");
+
 
 // PROMPT USER TO INPUT CITY
 function createEmptyPage(){
@@ -19,16 +22,23 @@ function createEmptyPage(){
     emptyMainEl.appendChild(promptEl);
 }
 
+// GET DESIRED CITY
 function getCityInput(e){
     e.preventDefault();
-    
-    var input = inputEl.value.trim()
+
+    var input = inputEl.value.trim();
     inputEl.value = "";
     if(input === ""){
         return;
     }else{
-        console.log(input);
+        getCityWeather(input);
+        console.log(moment().format("dddd, MMMM Do, YYYY, h:mm a"));
     }
+}
+
+function getCityWeather(cityName){
+    var apiUrl = "https://api.openweathermap.org/data/2.5/onecall/timemachine?lat=" + "43.65" + "&lon=" + "79.3" + "&dt=" + "minutely" + "&appid=" + "{20d2e8b234ab2aab32059e9826a39af0}";
+    console.log("apiUrl", apiUrl);
 }
 
 createEmptyPage();
